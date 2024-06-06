5da6705dc8c366c9355577ad54c21d13284f600a
Jyoti Thakur <jthakur777.jt@gmail.com>
2024-06-06 15:18:10 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import BookList from "./container/BookList";
 import BookShelf from "./container/BookShelf";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
+    <Box gap={2} px={8}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<BookList />} />
           <Route path="/my-books" element={<BookShelf />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </Box>
   );
 }
 
